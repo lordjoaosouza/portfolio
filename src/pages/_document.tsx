a918179bba1b42c74d7c6738dd71aeafb1ba4b96
lordjoaosouza <lordjoaosouza@protonmail.com>
2023-03-15 21:11:47 -0300

    ChakraUI, ESLint and Prettier setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head title='João Souza | Portfolio' />
       <body>
         <Main />
         <NextScript />
