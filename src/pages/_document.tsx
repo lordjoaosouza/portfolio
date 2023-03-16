39fd3ae5951b7e50a558efd870389d6c09fe09c5
lordjoaosouza <lordjoaosouza@protonmail.com>
2023-03-15 21:24:08 -0300

    Updating title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head title='João Souza | Portfolio' />
+      <Head>
+        <title>João Souza | Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
