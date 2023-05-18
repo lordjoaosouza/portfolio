ad60091d71c1107c050e586713a3bcdeba9a30ae
Joao Souza <lordjoaosouza@protonmail.com>
2023-05-18 18:38:33 -0300

    Updating Footer's responsivity

--- src/components/FooterIcon.tsx
+++ src/components/FooterIcon.tsx
@@ -9,7 +9,7 @@ interface FooterIconProps {
 const FooterIcon: React.FC<FooterIconProps> = ({ icon, target, href }) => {
   return (
     <a href={href} target={target}>
-      {React.cloneElement(icon, { className: 'w-4 h-4 sm:w-5 sm:h-5' })}
+      {React.cloneElement(icon, { className: 'w-4 h-4 sm:w-5 sm:h-5 mr-1 sm:mr-2' })}
     </a>
   )
 }
